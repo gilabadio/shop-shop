038baae4d06708ba52bae02293c507b24c226864
Gil B. Abadio <gilabadio@gmail.com>
2021-01-25 19:34:27 -0500

    added indexdb

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
 import { useStoreContext } from '../../utils/GlobalState';
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { idbPromise } from "../../utils/helpers";
 
 function ProductItem(item) {
   const {
@@ -33,8 +34,9 @@ function ProductItem(item) {
         type: ADD_TO_CART,
         product: { ...item, purchaseQuantity: 1 }
       });
+      idbPromise('cart', 'put', { ...item, purchaseQuantity:1 });
     }
-  };
+  }
   return (
     <div className="card px-1 py-1">
       <Link to={`/products/${_id}`}>
